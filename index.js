ddbba16955ffcb76baba6672a4b77adf6a84c86f
TechDetective <vishwajeetsinhjadeja4@gmail.com>
2017-06-15 11:21:39 +0530

    Movie API

--- index.js
+++ index.js
@@ -133,9 +133,9 @@ var getMovieDetail = function(userId, field){
 function findMovie(userId, movieTitle) {
   request("https://api.themoviedb.org/3/search/movie?api_key="+process.env.MOVIE_API_KEY+"&language=en-US&query="+movieTitle+"&page=1&include_adult=true" + movieTitle, function (error, response, body) {
     if (!error && response.statusCode === 200) {
-      console.log(body);
-      var movieObj = body;
-      if (movieObj.success) {
+      console.log(body.results);
+      var movieObj = body.results ? body.results[0] : null;
+      if (movieObj) {
 
         var query = {user_id: userId};
         var update = {
